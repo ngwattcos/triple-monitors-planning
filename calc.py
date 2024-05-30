97939c41a18143cc6647d126d5039827e64caee0
ngwattcos <curiosity142000@gmail.com>
2024-05-29 22:01:07 -0400

    convert to int to avoid type error

--- calc.py
+++ calc.py
@@ -1,16 +1,16 @@
 import math
 
-l = input("Monitor size (in, diag): ")
-z = input("monitor distance (in): ")
+l = int(input("Monitor size (in, diag): "))
+z = int(input("Monitor distance (in): "))
 
 w = 0.872 * l
 h = 0.49 * l
 phi = math.acos(2 * z / w)
 theta = 2 * phi
 
-total_width = w * (1 + math.cos(theta))
-total_length = w / 2 * math.sin(theta)
-vert_angle = math.degrees(2 * math.atan(h / (2 * z)))
+total_width = math.round(w * (1 + math.cos(theta)))
+total_length = math.round(w / 2 * math.sin(theta))
+vert_angle = math.round(math.degrees(2 * math.atan(h / (2 * z))))
 
 print("total_width: " + total_width)
 print("total_length: " + total_length)
