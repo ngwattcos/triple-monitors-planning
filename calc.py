02a25e4b11d68f22708f34aa83270c6c63fda376
ngwattcos <curiosity142000@gmail.com>
2024-05-29 21:58:40 -0400

    convert final output to degrees, correct syntax error

--- calc.py
+++ calc.py
@@ -3,15 +3,15 @@ import math
 l = input("Monitor size (in, diag): ")
 z = input("monitor distance (in): ")
 
-w = 0.872*l
+w = 0.872 * l
 h = 0.49 * l
 phi = math.acos(2 * z / w)
 theta = 2 * phi
 
 total_width = w * (1 + math.cos(theta))
 total_length = w / 2 * math.sin(theta)
-vert_angle = 2 * math.atan(h / (2 * z))
+vert_angle = math.degrees(2 * math.atan(h / (2 * z)))
 
-println("total_width: " + total_width)
-println("total_length: " + total_length)
-println("vert_angle: " + vert_angle)
+print("total_width: " + total_width)
+print("total_length: " + total_length)
+print("vert_angle: " + vert_angle)
