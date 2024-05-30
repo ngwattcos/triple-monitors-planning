c1a4add910a274b02f3ca13e24c2d13af8e748b8
ngwattcos <curiosity142000@gmail.com>
2024-05-29 22:09:43 -0400

    fix trig issues

--- calc.py
+++ calc.py
@@ -1,17 +1,18 @@
 import math
 
-l = int(input("Monitor size (in, diag): "))
-z = int(input("Monitor distance (in): "))
+l = float(input("Monitor size (in, diag): "))
+z = float(input("Monitor distance (in): "))
 
 w = 0.872 * l
 h = 0.49 * l
-phi = math.acos(2 * z / w)
+
+phi = math.atan(w /(2.0 * z))
 theta = 2 * phi
 
-total_width = math.round(w * (1 + math.cos(theta)))
-total_length = math.round(w / 2 * math.sin(theta))
-vert_angle = math.round(math.degrees(2 * math.atan(h / (2 * z))))
+total_width = round(w * (1 + math.cos(theta)))
+total_length = round(w / 2 * math.sin(theta))
+vert_angle = round(math.degrees(2 * math.atan(h / (2 * z))))
 
-print("total_width: " + total_width)
-print("total_length: " + total_length)
-print("vert_angle: " + vert_angle)
+print("total_width: " + str(total_width))
+print("total_length: " + str(total_length))
+print("vert_angle: " + str(vert_angle))
